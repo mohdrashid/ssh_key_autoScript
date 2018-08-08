ac50c6207045515cfd09de130d6cd5a213be1a9d
Mohammed Rashid <mohmad.rashid@hotmail.com>
2018-08-08 10:51:00 +0400

    Made compatible

--- script.sh
+++ script.sh
@@ -6,10 +6,9 @@ read username
 sudo adduser $username
 # Generating keys
 
-su $username && cd
-
 echo "/home/$username/.ssh/authorized_keys" | ssh-keygen -t rsa -p ""
 # Moving files to format required by system
-mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.ppk
-mv ~/.ssh/authorized_keys.pub ~/.ssh/authorized_keys
+mv /home/$username/.ssh/authorized_keys /home/$username/.ssh/authorized_keys.ppk
+mv /home/$username/.ssh/authorized_keys.pub /home/$username/.ssh/authorized_keys
+chown $username:$username -R /home/$username/.ssh 
 
